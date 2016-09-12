0b7246a1d77ce3a405a745a92ae3af86954921f9
Salvatore Gentile <salgnt@outlook.com>
2016-09-12 17:09:49 +0200

    Minor fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,13 @@
 class User < ActiveRecord::Base
+  has_many :vaults, :dependent => :delete_all
 
   VALID_USERNAME_REGEX = /\A[\w\s-]*\z/i
 
-  has_many :vaults, :dependent => :delete_all
-
   validates :username, presence: true, uniqueness: true, length: { in: 3..18 }, format: { with: VALID_USERNAME_REGEX }
-
-  has_attached_file :avatar, styles: { thumb: "100x100" },
-                    default_url: '/assets/default_avatar.png'
-
+  has_attached_file :avatar, styles: { thumb: "100x100" }, default_url: '/assets/default_avatar.png'
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :users, only: [:show]
+
   resources :vaults do
     resources :snippets
   end
